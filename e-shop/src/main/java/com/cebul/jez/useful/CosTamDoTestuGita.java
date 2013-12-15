7a0197c9386248109e9870c7b536b288f810b793
Mateusz <cebul4@gmail.com>
2013-12-15 14:57:36 +0100

    wrzucam klae dla testu

---
+++ e-shop/src/main/java/com/cebul/jez/useful/CosTamDoTestuGita.java
@@ -0,0 +1,16 @@
+package com.cebul.jez.useful;
+
+public class CosTamDoTestuGita {
+	private String lol;
+
+	public String getLol() {
+		return lol;
+	}
+
+	public void setLol(String lol) {
+		this.lol = lol;
+	}
+	
+	
+
+}
